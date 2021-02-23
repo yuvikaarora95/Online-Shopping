4de8250f3d5f71a86875b0dc23e60f6da37b17d1
yuvika arora <yuvikaarora1@gmail.com>
2021-02-23 16:35:29 -0500

    green color

--- login-register/src/App.js
+++ login-register/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import {useSpring, animated} from 'react-spring';
 
+
 function App() {
 
   const [registrationFormStatus, setRegistrationFormStatus] = useState(false);
@@ -19,6 +20,39 @@ function App() {
   function registerClicked() {  setRegistrationFormStatus(true)}
   function loginClicked() { setRegistrationFormStatus(false)}
 
+  
+
+  let record = [];
+ 
+    const addData = (ev) => {
+      ev.preventDefault();
+      let data = {
+        id: (((1+Math.random())*0x10000)|0).toString(16).substring(1),
+        name: document.getElementById('fullname').value,
+        email: document.getElementById('email').value,
+        address: document.getElementById('address').value,
+        phone: document.getElementById('phone').value,
+        rpassword: document.getElementById('rpassword').value
+      }
+      record.push(data);
+      //document.querySelector('form').reset();
+
+      console.log('added', {record});
+      let pre = document.querySelector('#msg pre');
+      if (pre){
+        pre.textContent = '\n' + JSON.stringify(record, '\t', 2);
+      }
+      localStorage.setItem('DataList', JSON.stringify(record));
+    }
+
+    
+
+
+  document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById('btn').addEventListener('click', addData);
+  });
+  
+  
 
   return (
     <div className="login-register-wrapper">
@@ -30,7 +64,7 @@ function App() {
         <animated.form action='' id='loginform' style={loginProps}><LoginForm/></animated.form> 
         <animated.form action='' id='registerform' style={registerProps}><RegisterForm/></animated.form>
       </div>
-      <animated.div className="forgot-panel" style={loginProps}><a href='#' >Forgot password?</a></animated.div>
+      <animated.div className="forgot-panel" style={loginProps}><a href="www.google.com" >Forgot password?</a></animated.div>
       
     </div>
   );
@@ -39,29 +73,31 @@ function App() {
 function LoginForm() {
   return (
     <React.Fragment>
-      <label for='username'>USERNAME</label>
+      <label htmlFor='username'>USERNAME</label>
       <input type='text' id='username'/>
-      <label for='password'>PASSWORD</label>
-      <input type='password' id='password'/>
-      <input type='submit' value='submit' className='submit'/>
+      <label htmlFor='password'>PASSWORD</label>
+      <input type='password' id='lpassword'/>
+      <input type='submit' value='submit' className='submit' />
     </React.Fragment>
   )}
 
 function RegisterForm() {
   return (
     <React.Fragment>
-      <label for='fullname'>FULLNAME</label>
-      <input type='text' id='fullname'/>
-      <label for='email'>EMAIL</label>
-      <input type='text' id='email' />
-      <label for='address'>ADDRESS</label>
-      <input type='text' id='address' />
-      <label for='phone'>PHONE</label>
-      <input type='text' id='phone'/>
-      <label for='password'>PASSWORD</label>
-      <input type='password' id='password'/>
-      <input type='submit' value='submit' className='submit'/>
-
+      <div id='form'>
+        <label htmlFor='fullname'>FULLNAME</label>
+        <input type='text' id='fullname'/>
+        <label htmlFor='email'>EMAIL</label>
+        <input type='text' id='email' />
+        <label htmlFor='address'>ADDRESS</label>
+        <input type='text' id='address' />
+        <label htmlFor='phone'>PHONE</label>
+        <input type='text' id='phone'/>
+        <label htmlFor='password'>PASSWORD</label>
+        <input type='password' id='rpassword'/>
+        <input type='submit' value='submit' className='submit' id='btn' />
+      </div>
+      
     </React.Fragment>
   ) }
 export default App;
